942b227a32c251f104dfd7d8ba422d59dea36d39
Jarota <32527884+Jarota@users.noreply.github.com>
2018-10-08 14:27:22 +0100

    first problem done

--- firstassignment/pterms.pl
+++ firstassignment/pterms.pl
@@ -2,5 +2,9 @@ pterm(null).
 pterm(f0(X)) :- pterm(X).
 pterm(f1(X)) :- pterm(X).
 
+same(null, null).
+same(f0(X), f0(Y)) :- same(X, Y).
+same(f1(X), f1(Y)) :- same(X, Y).
+
 incr(null, f1(null)).
-incr(P1, P2) :- incr(pterm(P1), pterm(P2)).
+incr(f0(X), f1(Y)) :- same(X, Y).
