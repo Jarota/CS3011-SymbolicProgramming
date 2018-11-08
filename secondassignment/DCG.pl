a2aa4987702d2cfbe82084d4b9dfc7d431f2e9f5
Jarota <32527884+Jarota@users.noreply.github.com>
2018-11-08 18:36:51 +0000

    sketch of q2

--- secondassignment/DCG.pl
+++ secondassignment/DCG.pl
@@ -1,7 +1,26 @@
-s --> [], l(0).
+%----- Q1 -----
+
+uv --> [], l(0).
 l(Count) --> [2], r(Count).
 l(Count) --> [0], l(succ(Count)).
 l(Count) --> [1], l(Count).
 r(0) --> [].
 r(Count) --> [0], r(Count).
 r(succ(succ(Count))) --> [1], r(Count).
+
+%----- Q2 -----
+
+s --> trio, trio, trio.
+trio --> col, nat, pet.
+
+col --> [red].
+col --> [blue].
+col --> [green].
+
+nat --> [english].
+nat --> [spanish].
+nat --> [japanese].
+
+pet --> [jaguar].
+pet --> [snail].
+pet --> [zebra].
