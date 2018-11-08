7a12135fc584fae5b30793a6f478490eb98478d9
Jarota <32527884+Jarota@users.noreply.github.com>
2018-11-08 18:13:18 +0000

    think i'm on the right track ...

--- secondassignment/DCG.pl
+++ secondassignment/DCG.pl
@@ -1,9 +1,6 @@
-s --> l, [2], r.
-l --> [0], l.
-l --> [1], l.
-l --> [0].
-l --> [1].
-r --> [0], r.
-r --> [1], r.
-r --> [0].
-r --> [1].
+s --> l(0).
+l(Count) --> [2], r(Count).
+l(Count) --> [0], l(succ(Count)).
+l(Count) --> [1], l(Count).
+r(Count) --> [0], r(Count).
+r(succ(succ(Count))) --> [1], r(Count).
